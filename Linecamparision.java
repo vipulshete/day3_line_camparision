3d51134e8a2a158a8ca6dc242130a7569bba4834
vipulshete <vipulshete724@gmail.com>
2022-07-16 13:06:12 +0530

    modified : measuring length

--- Linecamparision.java
+++ Linecamparision.java
@@ -4,7 +4,18 @@ public class Linecamparision {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Line camparision");
+		System.out.println("Welcome to Line camparisioncd");
+		
+		// All Values in mm.
+		
+		int x1 = 2; 
+		int y1 = 2; 
+		int x2 = 1; 
+		int y2 = 1; 
+		
+		double lengthOfLine = Math.sqrt((Math.pow((x2-x1),2))+ (Math.pow((y2-y1),2)));
+		System.out.println("Length of line is " + lengthOfLine);
+		
 	}
 
 }
