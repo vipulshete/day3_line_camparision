700f1e6a9783123054fb3b3469245a0089bf38f9
vipulshete <vipulshete724@gmail.com>
2022-07-16 12:56:10 +0530

    welcome to line camparision

---
+++ Linecamparision.java
@@ -0,0 +1,10 @@
+package com.brigelab.assignment.linecomparision;
+
+public class Linecamparision {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line camparision");
+	}
+
+}
