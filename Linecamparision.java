714d290cb2fe76ae2973e28c2547b4d9392647fc
vipulshete <vipulshete724@gmail.com>
2022-07-16 15:02:12 +0530

    modifies : line comparission

--- Linecamparision.java
+++ Linecamparision.java
@@ -8,21 +8,24 @@ public class Linecamparision {
 		
 		// All Values in mm.
 		
-				int x1 = 2; int a1 =4;
-				int y1 = 2; int a2 =2;
-				int x2 = 1; int b1 =2;
-				int y2 = 1; int b2 =1;
-			
-				int slopeline1 = (int) ((y2-y1)/(x2-x1));
-				System.out.println("slope of line one is " + slopeline1);
-				int slopeline2 = (int) ((b2-b1)/(a2-a1));
-				System.out.println("slope of line two is " + slopeline2);
-				
-				if(slopeline1==slopeline2) {
-					System.out.println("Line one and two are equal");
-				} else {
-					System.out.println("Line one and two are not equal");
-				}
+		int x1 = 2; int a1 =4;
+		int y1 = 2; int a2 =2;
+		int x2 = 1; int b1 =2;
+		int y2 = 1; int b2 =1; 
+		
+		int line1 = (int)Math.sqrt((Math.pow((x2-x1),2))+ (Math.pow((y2-y1),2)));
+		System.out.println("Length of line is " + line1 + " mm");
+		
+		int line2 = (int)Math.sqrt((Math.pow((a2-a1),2))+ (Math.pow((b2-b1),2)));
+		System.out.println("Length of line is " + line2 + " mm");
+		
+		if(line1 == line2) {
+			System.out.println("Both lines are same");
+		} else if (line1 < line2) {
+			System.out.println("Line one is less than line two");
+		} else {
+			System.out.println("Line one is greater than line two");
+		}
 	}
 
 }
